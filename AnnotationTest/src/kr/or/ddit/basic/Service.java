6b79775ecc31ea3726d2204776975ab076d10d41
PC-14 <PC-14@DESKTOP-KUCU871>
2021-12-16 19:14:34 +0900

    12-16

--- AnnotationTest/src/kr/or/ddit/basic/Service.java
+++ AnnotationTest/src/kr/or/ddit/basic/Service.java
@@ -1,16 +1,21 @@
 package kr.or.ddit.basic;
 
+
 public class Service {
+
 	@PrintAnnotation
 	public void method1() {
 		System.out.println("메서드1에서 출력되었습니다.");
 	}
-	@PrintAnnotation(value="%")
+	
+	@PrintAnnotation(value="%")     //value값 하나만 가지고 있을 경우 value생략가능
 	public void method2() {
 		System.out.println("메서드2에서 출력되었습니다.");
 	}
-	@PrintAnnotation(value="#", count=25)
+	
+	@PrintAnnotation(value="#", count= 25)     //생략 불가
 	public void method3() {
 		System.out.println("메서드3에서 출력되었습니다.");
 	}
+	
 }
